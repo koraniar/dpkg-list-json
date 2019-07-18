7c718f1916320cfe91bd07b6222aef76b3742804
Koraniar VP-002 U <esteban.garcia@tulpep.com>
2019-07-18 15:59:22 -0500

    Get installation date

--- dpkg_list_json.py
+++ dpkg_list_json.py
@@ -27,11 +27,27 @@ import json
 import sys
 
 def getSoftwareWithGUI():
-    lines = os.popen('dpkg --search "*.desktop" | awk "{print $1}" | sed "s/://" | cut -d " " -f 1 | sort --unique').read().split('\n')
+    return os.popen('dpkg --search "*.desktop" | cut -d ":" -f 1 | sort --unique').read().split('\n')
+
+def getSoftwareInstallationdate():
+    lines = os.popen('grep " installed " /var/log/dpkg.log').read().split('\n')
+    for i in range(len(lines)):
+        information = lines[i].split(' ')
+        if len(information) >= 4:
+            lines[i] = [information[4].split(':')[0], information[0] + ' ' + information[1]]
+        else:
+            lines[i] = ['', '']
     return lines
+
+def getInstallationDate(softwareName, installationDates):
+    for installationDate in installationDates:
+        if installationDate[0] == softwareName:
+            return installationDate[1]
+    return ''
 
 def main():
     mainPrograms = getSoftwareWithGUI()
+    installationDate = getSoftwareInstallationdate()
     lines = os.popen('dpkg -l | grep "^ii"').read().split('\n')
     i = 0
     while len([l for l in lines[i].split('  ') if l]) != 5:
@@ -47,12 +63,16 @@ def main():
                 parsed.append(line[offsets[i]:offsets[i + 1]].strip())
 
         if len(parsed[1]) > 0:
+            parsed.append(getInstallationDate(parsed[1], installationDate))
             try:
                 mainPrograms.index(parsed[1])
                 parsed.append(True)
             except:
                 parsed.append(False)
-            pkgs.append({'Name':parsed[1], 'State':parsed[0], 'Version':parsed[2], 'Architecture':parsed[3], 'Description':parsed[4], 'HasGUI':parsed[5]})
+            
+            pkgs.append({'Name':parsed[1], 'State':parsed[0], 'Version':parsed[2],
+            'Architecture':parsed[3], 'Description':parsed[4], 'HasGUI':parsed[6],
+            'InstallationDate':parsed[5]})
 
     json_output = json.dumps(pkgs)
 
